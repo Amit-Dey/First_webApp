a7f456bc98c91eebd6e2b371d70d6b4b26b1ddb8
Amit-Dey <amitdey9020@gmail.com>
2021-04-14 02:05:13 +0600

    New Task app added

--- First_webApp/urls.py
+++ First_webApp/urls.py
@@ -19,5 +19,6 @@ from django.urls import include,path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myweb/',include("Myweb.urls")),
-    path('newyear/',include("newyear.urls"))
+    path('newyear/',include("newyear.urls")),
+    path('tasks/',include("tasks.urls"))
 ]
